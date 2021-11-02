4a042a12d6f263558600e2a1e82cb54967678426
movindu <movindu@dyn3168-249.wlan.ic.ac.uk>
2021-11-02 11:48:38 +0000

    Added new implementation: -When bit pressed in PORTD, is stored in PORTE until it's pressed again through PORTD to change it again.

--- main.s
+++ main.s
@@ -10,8 +10,15 @@ main:
 start:
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
+	movwf	TRISE, A	    ; Port E is output
+	movlw	0xff
+	movwf	TRISD, A	    ; Port D is input
 	bra 	test
 loop:
+	movf	0x08, W		    ; Store register 0x08 in W
+	xorwf	PORTD, 0, 0	    ; XOR(PORTD, W) ie get the opposite bit 
+	movwf	0x08, A		    ; Store register W in 0x08
+	movwf	PORTE, A	    ; Output W into PORTE
 	movff 	0x06, PORTC
 	incf 	0x06, W, A
 test:
